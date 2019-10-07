debea49af34804f167e3cd10ac63320f5d2a88a9
Atayi <atayisika@gmail.com>
2019-10-07 15:36:55 +0200

    Insertion et mise en forme de la navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className='Navbar'>
+            <h1>
+                <a href='https://fr.wikipedia.org/wiki/Les_Simpson' target='_blank'>Simpsons quetes react</a>
+            </h1>
+        </nav>
+    );
+}
+export default Navbar;
