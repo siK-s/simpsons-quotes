32b0d1ad3dfa9321c59b54a1dbd60f3c1381f808
Atayi <atayisika@gmail.com>
2019-10-07 19:09:27 +0200

    Ajout de la citation d'un nouveau personnage des Simpsons

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard />
+      <QuoteCard image='https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185' quote="Shoplifting is a victimless crime, like punching someone in the dark." character="Nelson Muntz" />
+      <QuoteCard image='https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNickRiviera.png?1497567511084' quote="Inflammable means flammable? What a country!" character='Dr. Nick' />
     </div>
   );
 }
