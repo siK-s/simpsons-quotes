c55c7d35ac00ded6e7ca4996ad9d52533aa64958
Atayi <atayisika@gmail.com>
2019-10-09 07:56:16 +0200

    Affichages de la liste des citations des personnages de Simpsons avec la method 'map'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import QuoteCard from './components/QuoteCard';
+import QuoteList from './components/QuoteList';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard image='https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185' quote="Shoplifting is a victimless crime, like punching someone in the dark." character="Nelson Muntz" />
-      <QuoteCard image='https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNickRiviera.png?1497567511084' quote="Inflammable means flammable? What a country!" character='Dr. Nick' />
+      <QuoteList />
     </div>
   );
 }
