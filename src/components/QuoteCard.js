a82bac6af4baa9a5f663e3d1a1ecd68ea6e913ca
Atayi <atayisika@gmail.com>
2019-10-14 15:45:52 +0200

    Modification du composant QuoteCard pour pouvoir tagger une étoile sur une citation favorite

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './QuoteCard.css';
 
+class QuoteCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { favorite: false };
+    }
+    render() {
+
+        const tagFaforite = (e) => {
+            const newFavorite = !this.state.favorite;
+            this.setState({favorite: newFavorite});
+        };
+
+        return (
+            <figure className='QuoteCard'>
+                <img src={this.props.image} alt={this.props.character} />
+                <figcaption>
+                    <blockquote>{this.props.quote}</blockquote>
+                    <p>
+                        <cite>{this.props.character}</cite>
+                        <span className = {this.state.favorite ? 'is-favorite' : ''} onClick={tagFaforite}>&#9733;</span>
+                    </p>
+                </figcaption>
+            </figure>
+        );
+    }
+}
+/*
 function QuoteCard(props) {
     return (
         <figure className='QuoteCard'>
@@ -12,4 +39,5 @@ function QuoteCard(props) {
         </figure>
     );
 }
+*/
 export default QuoteCard;
